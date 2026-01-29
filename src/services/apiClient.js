f1c63295ae931e572764624228942e3e2137b5b8
Islam ElSaqqa <islamashrafmahmoud5@gmail.com>
2026-01-29 06:09:08 +0200

    Custom-Fetch-Promise

---
+++ src/services/apiClient.js
@@ -0,0 +1,18 @@
+const BASE_URL = "http://localhost:3000";
+
+//& Creating an API Custom Client for all CRUD Operations
+export const request = async (endpoint, options = {}) => {
+    
+    //? http://localhost:3000 + Endpoint
+    const response = await fetch(`${BASE_URL}${endpoint}`, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        ...options //& spreading options from params
+    })
+    if (!response.ok)
+        //? status = number
+        throw new Error(`API Error: ${response.status}`)
+    return response.json(); //& Parsing and return immediately
+
+};
