711c8fe5cb74e35e2c1b056ff53ef3dd0605e432
Islam ElSaqqa <islamashrafmahmoud5@gmail.com>
2026-02-05 14:14:11 +0200

    Removing-unneccessary-codes

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10,23 +10,6 @@ export function searchbyName(value, data) {
     return data.filter(d => d.name.toLowerCase().includes(value.toLowerCase()));
 }
 
-export const calculateStudentStats = (students) => {
 
-    const ages = students.map(s => s.age);
 
-    return {
-        total: students.length,
-        avgAge: (ages.reduce((a,b)=>a+b,0) / ages.length).toFixed(1),
-        youngest: Math.min(...ages),
-        oldest: Math.max(...ages)
-    };
-};
 
-export const getCounts = (data) => {
-
-    return {
-        studentCount: data.students.length,
-        instructorCount: data.instructors.length,
-        employeeCount: data.employees.length
-    };
-};
